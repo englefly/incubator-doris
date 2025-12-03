130a5cbf6ca9562b7786e6dbb6509e4f7ddbb919
minghong <zhouminghong@selectdb.com>
2025-12-03 15:19:39 +0800

    decimal-spec

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/eageraggregation/PushdownSumIfAggregation.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/rewrite/eageraggregation/PushdownSumIfAggregation.java
@@ -142,7 +142,7 @@ public class PushdownSumIfAggregation extends DefaultPlanRewriter<JobContext> im
                                         new If(
                                                 output.child(0).child(0).child(0),
                                                 toReplace,
-                                                output.child(0).child(0).child(2)
+                                                new NullLiteral(toReplace.getDataType())
                                     )
                             )
                         );
